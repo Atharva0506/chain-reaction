c82d6f6a39566a8c9f1be09aaa714afa18bfd463
Atharva Naik <atharvan.coder@gmail.com>
2024-07-05 10:34:33 +0530

    Trying To fix Front end Issues

--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -1,15 +1,26 @@
-
-import { useSocket } from '../hooks/useSocket'
+import { Button } from "../components/Button";
+import GameBoard from "../components/GameBoard";
+import { useSocket } from "../hooks/useSocket";
 
 const Game = () => {
-  const socket  =  useSocket()
+  const socket = useSocket();
 
-  
-
-  if(!socket) return <div>Connecting...</div>
+  // if(!socket) return <div>Connecting...</div>
   return (
-    <div>Game</div>
-  )
-}
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="flex flex-col lg:flex-row items-center justify-between w-full max-w-6xl p-4">
+        {/* Game Board */}
+        <div className=" p-8 shadow-lg mb-4 lg:mb-0 lg:mr-4 flex-1">
+          <h2 className="text-xl font-bold text-white">Game Board</h2>
+         <GameBoard/>
+        </div>
+        <div className="p-8 shadow-lg flex-1">
+          <h2 className="text-xl font-bold">Buttons</h2>
+          <Button > Play Now</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Game
+export default Game;
