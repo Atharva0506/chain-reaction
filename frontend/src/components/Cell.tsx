27724632fe51aaaac65565c7f24791e5adde2863
Atharva Naik <atharvan.coder@gmail.com>
2024-07-08 20:16:20 +0530

    Socket Issue :(

--- frontend/src/components/Cell.tsx
+++ frontend/src/components/Cell.tsx
@@ -7,14 +7,10 @@ interface CellProps {
 }
 
 const Cell: React.FC<CellProps> = ({ player, atoms, onClick }) => {
-  const playerColors = ['bg-white', 'bg-red-500', 'bg-blue-500'];
-
   return (
-    <div
-      className={`flex items-center justify-center border border-black w-12 h-12 cursor-pointer ${playerColors[player]} text-white`}
-      onClick={onClick}
-    >
-      {atoms}
+    <div className="cell" onClick={onClick}>
+      {/* Render cell content based on player and atoms */}
+      {atoms > 0 && <div>{`P${player}: ${atoms}`}</div>}
     </div>
   );
 };
